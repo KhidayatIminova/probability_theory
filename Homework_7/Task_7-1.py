da2336f52a196fef893deb385a2412da2227f9ee
Khidayat_Iminova <hidayatka89@gmail.com>
2023-02-09 22:14:45 +0700

    Fix tsk_7-1

--- Homework_7/Task_7-1.py
+++ Homework_7/Task_7-1.py
@@ -5,6 +5,13 @@
 # Решение:
 # 	Поскольку нарушено условие применимости t-критерия произведем тест Манна-Уитни с помощью функции mannwhitheyu().
 
+import numpy as np
+import scipy.stats as stats
 
+x1=np.array([380,420,290])
+y1=np.array([140,360,200,900])
+alpha=0.05
+stats.mannwhitneyu(x1, y1)
 
+# MannwhitneyuResult(statistic=8.0, pvalue=0.6285714285714286)
 # Т.к. p-value>α, (α=0,05) статистически значимых различий в выборках нет. Нулевую гипотезу H_0 не отвергаем
